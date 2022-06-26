19dbcc1b64f010050c98c380cedad4a270e51c71
popatre <76402245+popatre@users.noreply.github.com>
2022-06-26 22:19:46 +0100

    setup test db

--- model/db.js
+++ model/db.js
@@ -1,6 +1,24 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/wikiDB");
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+    path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!ENV) {
+    throw new Error("Database URI not set");
+}
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then((res) => {
+        const connectedDB = res.connections[0].name;
+        console.log(`Connection successful to DB - ${connectedDB}`);
+    })
+    .catch((err) => console.log(err));
+
+// mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 const { Schema } = mongoose;
 
